74163f02d23d485cb7a5b85528314d283c57db9e
San4e <user@example.com>
2026-06-15 22:19:56 +0700

    fix page date serialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import ExploreClient from './ExploreClient';
 export const revalidate = 0;
 
 export default async function ExplorePage() {
-  const initialProfiles = await getProfiles();
+  const rawProfiles = await getProfiles();
+  const initialProfiles = JSON.parse(JSON.stringify(rawProfiles));
 
   return (
     <div className="w-full h-full flex flex-col">
